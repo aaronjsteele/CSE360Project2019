4cf2230441edfaa04eca33acae438a2c5de51e61
CactusClimber <ajsteele@asu.edu>
2019-04-21 19:12:06 -0700

    Main UI elements implemented

--- src/todolist/Main.java
+++ src/todolist/Main.java
@@ -3,6 +3,8 @@ package todolist;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -19,6 +21,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("/todolist/todolist.fxml"));
         primaryStage.setTitle("To-Do List Unlimited 2019");
+        
+        String blackBorderStyle = "-fx-border-color: black";
         
         Button addButton = new Button("Add");
         Button printButton = new Button("Print");
@@ -37,18 +41,12 @@ public class Main extends Application {
         buttonBox.setPadding(new Insets(10, 10, 10, 10)); 
         buttonBox.getChildren().addAll(addButton, printButton, saveButton, loadButton);
         
-        
-        AnchorPane anchorCenter = new AnchorPane();
-        
-        ScrollPane scrollWindow = new ScrollPane();
         VBox scrollVBox = new VBox();
-       
-        scrollWindow.setContent(scrollVBox);
-        
-        scrollWindow.setPrefWidth(550);
-        scrollVBox.setFillWidth(true);
+        scrollVBox.setPrefWidth(550);
+        scrollVBox.setPrefHeight(500);
+        scrollVBox.setMaxHeight(500);
                 
-        for (int i = 1; i < 7; ++i) {
+        for (int i = 1; i < 5; ++i) {
         	Task newTask = new Task();
         	ToDoItem newToDoItem = new ToDoItem(newTask);
         	newToDoItem.setPrefWidth(550);
@@ -58,19 +56,39 @@ public class Main extends Application {
         	scrollVBox.getChildren().add(newToDoItem);
         }
         
-        anchorCenter.getChildren().add(scrollWindow);
-        AnchorPane.setTopAnchor(scrollWindow, 0.0);
-        AnchorPane.setBottomAnchor(scrollWindow, 0.0);
-        AnchorPane.setLeftAnchor(scrollWindow, 0.0);
-        AnchorPane.setRightAnchor(scrollWindow, 0.0);
-        
         VBox titleBox = new VBox();
         Label titleLabel = new Label("To-Do Items");
         
-        root.setLeft(buttonBox);
-        root.setCenter(anchorCenter);
+        // Creates the bottom portion of the main UI
+        HBox bottomSortBox = new HBox();
+        bottomSortBox.setPrefHeight(50);
+        bottomSortBox.setMaxHeight(50);
+        bottomSortBox.setMinHeight(50);
         
-        Scene scene = new Scene(root, 710, 650);
+        Label sortLabel = new Label("Sort By:");
+
+        ObservableList<String> sortOptions =
+        	FXCollections.observableArrayList(
+    			"Priority",
+    			"Due Date",
+    			"Name"
+        	);
+        final ComboBox sortComboBox = new ComboBox(sortOptions);
+        Region midSpaceRegion = new Region();
+        Button leftArrow = new Button("<");
+        Button rightArrow = new Button(">");
+        Label pageLabel = new Label("Showing 1-4 of 13");
+        
+        bottomSortBox.getChildren().addAll(sortLabel, sortComboBox,
+        		midSpaceRegion, leftArrow, pageLabel, rightArrow);
+        
+        root.setLeft(buttonBox);
+        root.setCenter(scrollVBox);
+        root.setBottom(bottomSortBox);
+        
+        scrollVBox.setStyle("-fx-border-color: black");
+        
+        Scene scene = new Scene(root, 710, 600);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
