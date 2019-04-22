4f9c9eb644798bf267b0a2f0b3c3ca066a5aea71
CactusClimber <ajsteele@asu.edu>
2019-04-21 22:26:45 -0700

    Added error dialog

---
+++ src/todolist/ErrorDialog.java
@@ -0,0 +1,47 @@
+package todolist;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ErrorDialog {
+	public static Stage createErrorDialog(String errorMessage) {
+		Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Error");
+        window.setWidth(300);
+        window.setHeight(100);
+        window.initStyle(StageStyle.UTILITY);
+        window.setResizable(false);
+        
+        Label errorLabel = new Label(errorMessage);
+        Button confirmButton = new Button("OK");
+        
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(10,10,10,10));
+        root.setSpacing(10);
+        root.getChildren().addAll(errorLabel, confirmButton);
+        
+        window.setScene(new Scene(root, 300, 100));
+        return window;
+	}
+}
